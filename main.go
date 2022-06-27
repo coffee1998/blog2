4fbc19f650466aca3b41d60e3426815cd39514f8
liangcaibing <liangcaibing@ghzhushou.com>
2022-06-27 14:29:44 +0800

    fixed

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	//fmt.Println("blog server start...")
 	//time.Sleep(30 * time.Second)
 	//fmt.Println("blog server end...")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-	//fmt.Println("start...")
-	//for i := 1; i <= 10; i++ {
-	//	fmt.Println(i)
-	//	time.Sleep(time.Second * 1)
-	//}
-	//fmt.Println("end")
+	//http.HandleFunc("/", handler)
+	//log.Fatal(http.ListenAndServe(":9999", nil))
+	fmt.Println("start...")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Second * 1)
+	}
+	fmt.Println("end")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
