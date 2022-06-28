a1c4fabb05fbb41477badd1659bc0a752d21eac5
liangcaibing <liangcaibing@ghzhushou.com>
2022-06-28 14:33:39 +0800

    getInstanceId

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -13,6 +14,7 @@ func main() {
 	//http.HandleFunc("/", handler)
 	//log.Fatal(http.ListenAndServe(":9999", nil))
 	fmt.Println("start...")
+	getInstanceId()
 	for i := 1; i <= 10; i++ {
 		fmt.Println(i)
 		time.Sleep(time.Second * 1)
@@ -22,4 +24,19 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func getInstanceId() {
+	url := "http://100.100.100.200/latest/meta-data/instance-id"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		bs, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(bs))
+		}
+	}
 }
