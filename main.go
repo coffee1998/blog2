16d41b6b53eaec7a2d80f9c94942d9ba25566ca0
liangcaibing <liangcaibing@ghzhushou.com>
2022-06-29 14:22:09 +0800

    getInstanceId

--- main.go
+++ main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
 	//fmt.Println("blog server start...")
 	//time.Sleep(30 * time.Second)
 	//fmt.Println("blog server end...")
-	//http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":9999", nil))
-	fmt.Println("start...")
-	getInstanceId()
-	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
-		time.Sleep(time.Second * 1)
-	}
-	fmt.Println("end")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+	//fmt.Println("start...")
+	//getInstanceId()
+	//for i := 1; i <= 10; i++ {
+	//	fmt.Println(i)
+	//	time.Sleep(time.Second * 1)
+	//}
+	//fmt.Println("end")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	getInstanceId()
+	w.WriteHeader(200)
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
